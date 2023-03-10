fbaa52e026341473b6c89512b3cf491128b4ebc4
Yuki Okuno <y.okuno.btm@gmail.com>
2023-03-10 10:29:27 +0900

    セキュリティグループを追加

---
+++ app/security-group.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "webserverSG" {
+  name   = "webserverSG"
+  vpc_id = aws_vpc.myvpc01.id
+
+  tags = {
+    Name = "web-server-sg"
+  }
+}
+
+# インバウンドルール
+resource "aws_security_group_rule" "in_ssh" {
+  aws_security_group_id = aws_security_group.webserverSG.id
+  type                  = "ingress"
+  cidr_block = [
+    "0.0.0.0/0"
+  ]
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp"
+}
+
+# アウトバウンドルール
+resource "aws_security_group_rule" "out_all" {
+  aws_security_group_id = aws_security_group.webserverSG.id
+  type                  = "egress"
+  cidr_block = [
+    "0.0.0.0/0"
+  ]
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+}
