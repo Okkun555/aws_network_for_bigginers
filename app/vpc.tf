93b5355049afafb9daca30952066a4225ea5a99c
Yuki Okuno <y.okuno.btm@gmail.com>
2023-03-15 09:48:12 +0900

    ec2をインターネット接続可能にする

--- app/vpc.tf
+++ app/vpc.tf
@@ -23,3 +23,36 @@ resource "aws_subnet" "mysubnet01" {
     Name = "my-subnet-01"
   }
 }
+
+# --------------------------------------------------------------
+# Internet Gateway
+# --------------------------------------------------------------
+resource "aws_internet_gateway" "myig" {
+  vpc_id = aws_vpc.myvpc01.id
+
+  tags = {
+    Name = "myig"
+  }
+}
+
+# --------------------------------------------------------------
+# Route Table
+# --------------------------------------------------------------
+resource "aws_route_table" "inettable" {
+  vpc_id = aws_vpc.myvpc01.id
+  
+  tags = {
+    Name = "inettable"
+  }
+}
+
+resource "aws_route" "inettable" {
+  route_table_id         = aws_route_table.inettable.id
+  gateway_id             = aws_internet_gateway.myig.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "mysubnet01_route" {
+  subnet_id      = aws_subnet.mysubnet01.id
+  route_table_id = aws_route_table.inettable.id
+}
