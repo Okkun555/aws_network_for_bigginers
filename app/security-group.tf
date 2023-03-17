4b7c90f98b002f3a727b177a1106cdfe8df56629
Yuki Okuno <y.okuno.btm@gmail.com>
2023-03-17 09:48:11 +0900

    security group設定にhttp, httpsを許容するように設定

--- app/security-group.tf
+++ app/security-group.tf
@@ -11,12 +11,28 @@ resource "aws_security_group" "webserverSG" {
 resource "aws_security_group_rule" "in_ssh" {
   security_group_id = aws_security_group.webserverSG.id
   type              = "ingress"
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
-  from_port = 22
-  to_port   = 22
-  protocol  = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+}
+
+resource "aws_security_group_rule" "allow_http" {
+  security_group_id = aws_security_group.webserverSG.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+}
+
+resource "aws_security_group_rule" "allow_https" {
+  security_group_id = aws_security_group.webserverSG.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
 }
 
 # アウトバウンドルール
